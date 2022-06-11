fbb8d9330ea375d86134815adefcb37ddf7c4688
NataliaGosc <natalia.goscinna@gmail.com>
2022-06-11 19:23:51 +0200

    initial

---
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -0,0 +1,69 @@
+import { FlatList, TouchableOpacity } from "react-native";
+import styled from "styled-components/native";
+import React, { useContext, useState } from "react";
+import { ActivityIndicator, Colors } from "react-native-paper";
+
+import { SafeArea } from "../../../components/utility/safe-area.component";
+import { Search } from "../components/search.component";
+import { Spacer } from "../../../components/spacer/spacer.component";
+import { RestaurantsContext } from "../../../services/restaurants/restaurants.context";
+import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
+import { FavouritesBar } from "../../../components/favourites/favourites-bar.component";
+import { FavouritesContext } from "../../../services/favourites/favourites.context";
+
+const RestaurantList = styled(FlatList).attrs({
+  contentContainerStyle: {
+    padding: 16,
+  },
+})``;
+
+const Loading = styled(ActivityIndicator)`
+  margin-left: -25px;
+`;
+const LoadingContainer = styled.View`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+`;
+
+export const RestaurantsScreen = ({ navigation }) => {
+  const { isLoading, restaurants } = useContext(RestaurantsContext);
+  const { favourites } = useContext(FavouritesContext);
+  const [isToggled, setIsToggled] = useState(false);
+
+  return (
+    <SafeArea>
+      {isLoading && (
+        <LoadingContainer>
+          <Loading size={50} animating={true} color={Colors.blue300} />
+        </LoadingContainer>
+      )}
+      <Search
+        isFavouritesToggled={isToggled}
+        onFavouritesToggle={() => setIsToggled(!isToggled)}
+      />
+      {isToggled && (
+        <FavouritesBar favourites={favourites} onNavigate={navigation.navigate} />
+      )}
+      <RestaurantList
+        data={restaurants}
+        renderItem={({ item }) => {
+          return (
+            <TouchableOpacity
+              onPress={() =>
+                navigation.navigate("RestaurantDetail", {
+                  restaurant: item,
+                })
+              }
+            >
+              <Spacer position="bottom" size="large">
+                <RestaurantInfoCard restaurant={item} />
+              </Spacer>
+            </TouchableOpacity>
+          );
+        }}
+        keyExtractor={(item) => item.name}
+      />
+    </SafeArea>
+  );
+};;
---
+++ src/services/restaurants/mock/index.js
@@ -0,0 +1,28 @@
+import antwerp from "./antwerp.json";
+import chicago from "./chicago.json";
+import toronto from "./toronto.json";
+import san_francisco from "./san_francisco.json";
+
+export const mocks = {
+  "51.219448,4.402464": antwerp,
+  "43.653225,-79.383186": toronto,
+  "41.878113,-87.629799": chicago,
+  "37.7749295,-122.4194155": san_francisco,
+};
+
+export const mockImages = [
+  "https://cdn.pixabay.com/photo/2015/04/08/13/13/food-712665__340.jpg",
+  "https://cdn.pixabay.com/photo/2017/02/15/10/57/pizza-2068272__340.jpg",
+  "https://cdn.pixabay.com/photo/2017/03/23/19/57/asparagus-2169305__340.jpg",
+  "https://cdn.pixabay.com/photo/2018/07/18/19/12/pasta-3547078__340.jpg",
+  "https://cdn.pixabay.com/photo/2016/03/27/22/38/cake-1284548__340.jpg",
+  "https://cdn.pixabay.com/photo/2017/03/31/18/02/strawberry-dessert-2191973__340.jpg",
+  "https://cdn.pixabay.com/photo/2016/11/18/14/39/beans-1834984__340.jpg",
+  "https://cdn.pixabay.com/photo/2017/03/17/10/29/coffee-2151200__340.jpg",
+  "https://cdn.pixabay.com/photo/2017/01/30/13/49/pancakes-2020863__340.jpg",
+  "https://cdn.pixabay.com/photo/2016/06/06/18/29/meat-skewer-1440105__340.jpg",
+  "https://cdn.pixabay.com/photo/2016/11/29/11/15/fruits-1869132__480.jpg",
+  "https://cdn.pixabay.com/photo/2016/11/18/15/31/cooking-1835369__340.jpg",
+  "https://cdn.pixabay.com/photo/2014/05/05/19/52/charcuterie-338498__340.jpg"
+
+];
